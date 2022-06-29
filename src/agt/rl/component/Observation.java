6c14f500b2ce57145855b1f7525aa8ee4bed984f
Robson Zagre Junior <r.zagre.jr@gmail.com>
2022-06-29 00:15:59 -0300

    Implement a funcional QLearning

--- src/agt/rl/component/Observation.java
+++ src/agt/rl/component/Observation.java
@@ -82,14 +82,12 @@ public class Observation implements Serializable{
 	public void setParamValues(Term observationTerm) {
 		Literal observation = (Literal) observationTerm;
 		int index = 0;
-        //FIXME
-        System.out.println("\n----->");
-        System.out.println(parameters);
-        System.out.println(observation.getTerms());
-		for(Term param : observation.getTerms()) {
-			parameters.get(index).setValue(param.toString());
-			index++;
-		}
+        if (observation.getTerms() != null) {
+            for(Term param : observation.getTerms()) {
+                parameters.get(index).setValue(param.toString());
+                index++;
+            }
+        }
 	}
 	
 	public void clearValues() {
